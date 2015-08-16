7d5181534e06cb06286c185243283e0967255aed
DirkCotton <dirkcotton@gmail.com>
2015-08-16 16:34:32 -0400

    Remove uninteresting accounts from trans data frame, remove Credit Card Payments

--- Input Account Transactions.R
+++ Input Account Transactions.R
@@ -12,3 +12,24 @@ trans$Original.Description <- lapply(trans$Original.Description, as.character) #
 #
 # assume Mint.com provides tranasctions from most recent date to oldest
 #
+
+# Convert to data frame
+trans <- data.frame(trans)
+
+# Select only accounts of interest
+
+include.accts <- c("BankAmericard Cash Rewards Signature Visa","Chase VISA","COLLEGE CARD","Costco AmEx","Dirks Rev Trust","Eric", "Fidelity Investments Investment Rewards Signature Visa","James D Cotton, UA 12 09 1999 James Dirk Cotton Revocable Trust Trust Brokerage Account","JDC Planning","Schwab","Vickis Rev Trust","Wells Fargo Checking Acct")
+y <- trans[trans$Account.Name %in% include.accts,]
+
+### add a warning for rejected account names!!!!!!!!!!!!
+
+## separate credit card payments into another vector, remove from trans
+
+# cc <- trans[(trans$Category="Credit Card Payment"),]
+#trans <- trans[!(trans$Category=="Credit Card Payment"),]
+
+cc <- trans[trans$Category == "Credit Card Payment",]
+trans <- trans[trans$Category != "Credit Card Payment",]
+
+
+
